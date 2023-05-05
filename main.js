0b210aa9e65584372dcfb1f35f0ea483b619de7c
shineking <zoey.shineking@gmail.com>
2023-05-04 23:04:24 -0400

    update the nav style

--- main.js
+++ main.js
@@ -3,7 +3,31 @@ document.addEventListener('DOMContentLoaded', () => {
   const nav = document.getElementById('nav');
 
   hamburgerMenu.addEventListener('click', () => {
+    //event.stopPropagation();
     nav.classList.toggle('open');
   });
+
+  const links = document.querySelectorAll('#nav a');
+  links.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      links.forEach((otherLink) => {
+        otherLink.classList.remove('selected');
+        otherLink.parentElement.classList.remove('selected');
+      });
+
+      link.classList.add('selected');
+      link.parentElement.classList.add('selected');
+    });
+  });
+
+  // document.addEventListener('click', function (event) {
+  //   const navElement = document.getElementById('nav');
+  //   if (navElement.contains(event.target)) {
+  //     return;
+  //   }
+  //   navElement.classList.remove('open');
+  // });
+
 });
+
 
